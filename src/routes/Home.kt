4f32b4457f544bc19b2190e04940892c416ff593
Saurabh Mathur <saurabhmathurrock@gmail.com>
2019-08-07 19:25:54 +0530

    Add route dependency

--- src/routes/Home.kt
+++ src/routes/Home.kt
@@ -1,7 +1,9 @@
 package com.saurabh.routes
 
 import io.ktor.application.call
+import io.ktor.response.respondText
 import io.ktor.routing.Route
+import io.ktor.routing.get
 
 const val HOME = "/"
 
